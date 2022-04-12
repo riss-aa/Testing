575912d57bf3de63457893dd765fb332cae427d2
ahmadamrullah26 <ahmadamrullah0995@gmail.com>
2022-04-12 13:44:09 +0700

    1

---
+++ database/migrations/2022_04_12_064054_create_dokter_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDokterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Dokter', function (Blueprint $table) {
+            $table->int("id", 11);
+            $table->varchar("kode", 10);
+            $table->varchar("Nama",100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Dokter');
+    }
+}
